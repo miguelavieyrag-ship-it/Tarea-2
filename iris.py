d3731996dff5f4a7aa2ec646ba21e4c0818f1fdd
miguelavieyrag-ship-it <miguel.a.vieyra.g@gmail.com>
2026-08-08 14:32:27 -0600

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,67 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+
+from sklearn import datasets
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+)
+
+
+st.write(''' # Clasificación con el dataset Iris ''')
+st.image("Iris1.jpg", caption="Iris morada.")
+
+st.header('Datos de evaluación')
+
+def user_input_features():
+  # Entrada
+  #Pclass = st.number_input('Clase:', min_value=1, max_value=3, value = 1, step = 1)
+  #Sex = st.number_input('Género:', min_value=0, max_value=1, value = 0, step = 1)
+  #Age = st.number_input('Edad:', min_value=0, max_value=100, value = 0, step = 1)
+  #SibSp = st.number_input('Hermanos(as)/Esposo(a):',min_value=0, max_value=10, value = 0, step = 1)
+  #Parch = st.number_input('Padres/Hijos:', min_value=0, max_value=10, value = 0, step = 1)
+  #Fare = st.number_input('Tarifa:')
+  #Embarked = st.number_input('Lugar de Embarque:', min_value=0, max_value=2, value = 0, step = 1)
+  sepal_length_cm = st.number_input('Longitud del Zepalo:', min_value=0.5, max_value=5.0, value = 0.5, step = 0.5)
+  sepal_width_cm = st.number_input('Ancho del Zepalo:', min_value=1.0, max_value=5.0, value = 0.5, step = 0.5)
+  petal_length_cm = st.number_input('Longitud del petalo:', min_value=1.0, max_value=7.0, value = 0.5, step = 0.5)
+  petal_width_cm = st.number_input('Ancho del petalo:',min_value=0.5, max_value=3.0, value = 0.5, step = 0.5)
+  #user_input_data = {'Pclass': Pclass,
+   #                  'Sex': Sex,
+    #                 'Age': Age,
+     #                'SibSp': SibSp,
+      #               'Parch': Parch,
+       #              'Fare': Fare,
+        #             'Embarked': Embarked}
+  user_input_data = {'sepal_length_cm': sepal_length_cm,
+                    'sepal_width_cm': sepal_width_cm,
+                    'petal_length_cm': petal_length_cm,
+                    'petal_width_cm': petal_width_cm,}
+
+  features = pd.DataFrame(user_input_data, index=[0])
+
+  return features
+
+df = user_input_features()
+
+iris =  pd.read_csv('iris1.csv', encoding='latin-1')
+
+
+classifier = DecisionTreeClassifier(max_depth=8, criterion='entropy', min_samples_leaf=10, max_features=7, random_state=0)
+classifier.fit(X, Y)
+
+prediction = classifier.predict(df)
+
+st.subheader('Predicción de tipo de iris')
+if prediction == 0:
+  st.write('setosa')
+elif prediction == 1:
+  st.write('versicolor')
+else:
+  st.write('virginica')
